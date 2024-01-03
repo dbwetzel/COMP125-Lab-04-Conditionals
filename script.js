75c6334fb1b42d6b55935f5b9e9e7238fb5eb2cc
David B Wetzel <david@familycatproductions.com>
2024-01-03 10:08:05 -0600

    Upload files

---
+++ script.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(320, 240);
+}
+
+function draw() {
+  background(200);
+  
+  // check to condition of the mouse
+  if(mouseIsPressed){ // true or false?
+    // if true, change the fill color here
+    
+  }
+  else { 
+    // otherwise do this:
+    
+  }
+  ellipse(width/2, height/2, 100, 100);
+}
